effe83500d70c9dbc0c50f9bb14c4b77912d2e3a
Mallaury <mallaury.ratajczyk@gmail.com>
2024-10-02 15:38:27 +0200

    change back

--- components/Home.js
+++ components/Home.js
@@ -38,7 +38,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymoviz-backend-gules-eta.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         console.log(data.movies);
